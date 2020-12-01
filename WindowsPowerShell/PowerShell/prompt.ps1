1d5674df9995da848600bf57d6addea0c25704ae
NaokiM03 <naoki.matsumura@sansan.com>
2020-12-02 05:25:30 +0900

    feat: simplify prompt

--- WindowsPowerShell/PowerShell/prompt.ps1
+++ WindowsPowerShell/PowerShell/prompt.ps1
@@ -15,45 +15,17 @@ function Get-CurrentGitBranch {
 function Get-Prompt {
     $color = Get-PromptColor
 
-    $currentPath = Convert-Path .
-    $splitedCurrentPath = $currentPath.split("\")
+    $path = Convert-Path .
+    $gitBranchName = Get-CurrentGitBranch
 
-    $str = ""
-
-    if ($splitedCurrentPath[1] -eq "Users") {
-        if ($splitedCurrentPath.length -eq 3) {
-            $str = "[ " + $splitedCurrentPath[2] + " ]"
-        } elseif ($splitedCurrentPath.length -eq 4) {
-            if ($splitedCurrentPath[3] -eq "Desktop" -or
-                    $splitedCurrentPath[3] -eq "Documents" -or
-                    $splitedCurrentPath[3] -eq "Pictures" -or
-                    $splitedCurrentPath[3] -eq "Downloads") {
-                $str = "[ " + $splitedCurrentPath[3] + " ]"
-            }
-        }
-    } elseif ($splitedCurrentPath[1] -eq "bin" -and $splitedCurrentPath.length -eq 2) {
-        $str = "[ " + $splitedCurrentPath[1] + " ]"
-    } elseif ($splitedCurrentPath[1] -eq "lib") {
-        if ($splitedCurrentPath.length -eq 2) {
-            $str = "[ " + $splitedCurrentPath[1] + " ]"
-        } else {
-            $splitedCurrentPath = $splitedCurrentPath[1..($splitedCurrentPath.length - 1)]
-            $str = "[ " + ($splitedCurrentPath -join "\") + " ]"
-        }
-    } elseif ($splitedCurrentPath[1] -eq "github" -or $splitedCurrentPath[1] -eq "gitlab") {
-        if ($splitedCurrentPath.length -eq 2) {
-            $str = "[ " + $splitedCurrentPath[1] + " ]"
-        } else {
-            $path = $splitedCurrentPath[1..($splitedCurrentPath.length - 1)]
-            $gitBranchName = Get-CurrentGitBranch
-            $str = "[ " + ($path -join "\") + " ] on " + $gitBranchName
-        }
+    $label = ""
+    if ($gitBranchName -eq $null) {
+        $label = "[ " + $path + " ]" + "`r`n>"
+    } else {
+        $label = "[ " + $path + " ]" + "`r`n" +
+            "on " + $gitBranchName + "`r`n>"
     }
 
-    if ($str -eq "") {
-        $str = "[ " + $currentPath + " ]"
-    }
-
-    Write-Host ($str + "`r`n>") -NoNewLine -ForegroundColor $color
+    Write-Host $label -NoNewLine -ForegroundColor $color
     return " "
 }
