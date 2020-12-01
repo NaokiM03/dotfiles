91357fc68c870f23a549f10b6f6ecd4ec0211cf0
NaokiM03 <naoki.matsumura@sansan.com>
2020-12-02 05:36:08 +0900

    feat: import private dotfiles

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -1,7 +1,12 @@
 # PowerShell フォルダ配下にある .ps1 ファイルをすべて読み込む処理
-$libDirectory = [Environment]::GetFolderPath("MyDocument") + "\WindowsPowerShell\PowerShell\"
-cd $libDirectory
+$modules = [Environment]::GetFolderPath("MyDocument") + "\WindowsPowerShell\PowerShell\"
+cd $modules
 ls -r ".\" | ? Extension -eq ".ps1" | % { .$_.FullName }
+
+$privateDotFile = "\dotfiles"
+if (Test-Path $privateDotFile) {
+    ls -r "\dotfiles" | ? Extension -eq ".ps1" | % { .$_.FullName }
+}
 
 # 初期フォルダの初期化
 home
